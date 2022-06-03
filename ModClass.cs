4d6294f05e9f79c6a9ab92e14c86b12345a9ade3
Will <makeithardcore@gmail.com>
2022-06-03 02:47:19 -0400

    Add enable menu

--- ModClass.cs
+++ ModClass.cs
@@ -9,17 +9,48 @@ using HutongGames.PlayMaker.Actions;
 
 namespace FallDamage
 {
-    public class FallDamage : Mod
+    public class FallDamage : Mod, IMenuMod
     {
+        private bool modenabled = true;
+
+        public bool ToggleButtonInsideMenu => throw new NotImplementedException();
+
         new public string GetName() => "FallDamage";
         public override string GetVersion() => "0.1.0";
+        public List<IMenuMod.MenuEntry> GetMenuData(IMenuMod.MenuEntry? toggleButtonEntry)
+        {
+            return new List<IMenuMod.MenuEntry>
+            {
+                new IMenuMod.MenuEntry
+                {
+                    Name = "Fall Damage Active",
+                    Description = null,
+                    Values = new string[]
+                    {
+                        "ON",
+                        "OFF"
+                    },
+                    Saver = opt => this.modenabled = opt switch
+                    {
+                        0 => true,
+                        1 => false,
+                        _ => throw new InvalidOperationException()
+                    },
+                    Loader = () => this.modenabled switch
+                    {
+                        true => 0,
+                        false => 1
+                    }
+                },
+            };
+        }
         public override void Initialize()
         {
             ModHooks.HeroUpdateHook += OnHeroUpdate;
         }
         public void OnHeroUpdate()
         {
-            if (HeroController.instance.hero_state == ActorStates.hard_landing)
+            if (HeroController.instance.hero_state == ActorStates.hard_landing && modenabled)
             {
                 HeroController.instance.TakeDamage(null, GlobalEnums.CollisionSide.other, 1, 1);
             }
